c21795547a9f426384a75bf109bc3cb5caaabc22
Josemiruta <josezarzuelagarcia@gmail.com>
2025-06-11 16:35:45 +0200

    fix: habilita conexión SSL para MySQL en Azure

--- src/app/api/_db.js
+++ src/app/api/_db.js
@@ -9,9 +9,10 @@ if (process.env.NODE_ENV !== 'production') {
 export function getMysqlConfig() {
   return {
     host: process.env.DB_HOST,
-    user: process.env.DB_USER,         // <-- Cambia a DB_USER
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,    // <-- Cambia a DB_NAME
+    database: process.env.DB_NAME,
     port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
+    ssl: { rejectUnauthorized: false }
   };
 }
