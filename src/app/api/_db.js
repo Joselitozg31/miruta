4fbd26fd2c8ee39ca259796a2a8683669f10020a
Josemiruta <josezarzuelagarcia@gmail.com>
2025-06-11 16:00:20 +0200

    fix: quitado localhost etc en Azure

--- src/app/api/_db.js
+++ src/app/api/_db.js
@@ -8,10 +8,10 @@ if (process.env.NODE_ENV !== 'production') {
 // Configuración de conexión a MySQL
 export function getMysqlConfig() {
   return {
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',         // <-- Cambia a DB_USER
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'miruta',    // <-- Cambia a DB_NAME
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,         // <-- Cambia a DB_USER
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,    // <-- Cambia a DB_NAME
     port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
   };
 }
